ae9eb8c945ac47c186b909967ce0fcfbb5d2d87e
Ryan Schesnuk <ryanschesnuk@gmail.com>
2017-09-30 15:16:40 -0600

    Move Counter state up to Application level

--- app.jsx
+++ app.jsx
@@ -32,15 +32,16 @@ Header.propTypes = {
 function Counter(props) {
   return (
     <div className="counter">
-      <button className="counter-action decrement"> - </button>
+      <button className="counter-action decrement" onClick={function() {props.onChange(-1);}}> - </button>
       <div className="counter-score"> {props.score} </div>
-      <button className="counter-action increment"> + </button>
+      <button className="counter-action increment" onClick={function() {props.onChange(1);}}> + </button>
     </div>
   );
 }
 
 Counter.propTypes = {
-  score: React.PropTypes.number.isRequired
+  score: React.PropTypes.number.isRequired,
+  onChange: React.PropTypes.func.isRequired,
 }
 
 function Player(props) {
@@ -50,7 +51,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className="player-score">
-        <Counter score={props.score} />
+        <Counter score={props.score} onChange={props.onScoreChange} />
       </div>
     </div>
   );
@@ -58,7 +59,8 @@ function Player(props) {
 
 Player.proptypes = {
   name: React.PropTypes.string.isRequired,
-  score: React.PropTypes.number.isRequired
+  score: React.PropTypes.number.isRequired,
+  onScoreChange: React.PropTypes.func.isRequired,
 }
 
 var Application = React.createClass({
@@ -82,21 +84,31 @@ var Application = React.createClass({
     };
   },
 
+  onScoreChange: function(index, delta) {
+    console.log('onScoreChange', index, delta);
+    this.state.players[index].score += delta;
+    this.setState(this.state);
+  },
+
   render: function() {
     return (
       <div className="scoreboard">
         <Header title={this.props.title} />
 
         <div className="players">
-          {this.state.players.map(function(player){
-            return <Player name={player.name} score={player.score} key={player.id} />
-          })}
+          {this.state.players.map(function(player, index) {
+            return (
+              <Player
+                onScoreChange={function(delta) {this.onScoreChange(index, delta)}.bind(this)}
+                name={player.name}
+                score={player.score}
+                key={player.id} />
+            );
+          }.bind(this))}
         </div>
       </div>
     );
   }
 });
-
-
 
 ReactDOM.render(<Application initialPlayers={PLAYERS} />, document.getElementById('container'));
