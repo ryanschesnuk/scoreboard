392580bcecdc4f571cb05fcc3f3dd02136118933
Ryan Schesnuk <ryanschesnuk@gmail.com>
2017-09-30 09:21:27 -0600

    Convert functional Counter component to component class

--- app.jsx
+++ app.jsx
@@ -28,19 +28,21 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
-function Counter(props) {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <div className="counter-score"> {props.score} </div>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
-}
+var Counter = React.createClass({
+  propTypes: {
+    score: React.PropTypes.number.isRequired
+  },
+  render: function() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <div className="counter-score"> {this.props.score} </div>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  }
+});
 
-Counter.proptypes = {
-  score: React.PropTypes.number.isRequired
-}
 
 function Player(props) {
   return (
