54c69a960f64d653405bd495454df6a925c2f5a4
Ryan Schesnuk <ryanschesnuk@gmail.com>
2017-10-01 21:59:31 -0600

    Create add/remove Player functionality

--- app.jsx
+++ app.jsx
@@ -16,6 +16,42 @@ var PLAYERS = [
   }
 ]
 
+var nextId = 4;
+
+var AddPlayerForm = React.createClass({
+  propTypes: {
+    onAdd: React.PropTypes.func.isRequired,
+  },
+
+  getInitialState: function() {
+    return {
+      name: "",
+    };
+  },
+
+  onNameChange: function(e) {
+    this.setState({name: e.target.value});
+  },
+
+  onSubmit: function(e) {
+    e.preventDefault();
+
+    this.props.onAdd(this.state.name);
+    this.setState({name: ""});
+  },
+
+  render: function() {
+    return (
+      <div className="add-player-form">
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.name} onChange={this.onNameChange}/>
+          <input type="submit" value="Add Player" />
+        </form>
+      </div>
+    )
+  }
+});
+
 function Stats(props) {
   var totalPlayers = props.players.length;
   var totalPoints = props.players.reduce(function(total, player){
@@ -76,6 +112,7 @@ function Player(props) {
   return (
     <div className="player">
       <div className="player-name">
+        <a className="remove-player" onClick={props.onRemove}>X</a>
         {props.name}
       </div>
       <div className="player-score">
@@ -89,6 +126,7 @@ Player.proptypes = {
   name: React.PropTypes.string.isRequired,
   score: React.PropTypes.number.isRequired,
   onScoreChange: React.PropTypes.func.isRequired,
+  onRemove: React.PropTypes.func.isRequired,
 }
 
 var Application = React.createClass({
@@ -118,6 +156,22 @@ var Application = React.createClass({
     this.setState(this.state);
   },
 
+  onPlayerAdd: function(name) {
+    console.log('Player Added:', name);
+    this.state.players.push({
+      name: name,
+      score: 0,
+      id: nextId,
+    });
+    this.setState(this.state);
+    nextId += 1;
+  },
+
+  onRemovePlayer: function(index) {
+    this.state.players.splice(index, 1);
+    this.setState(this.state);
+  },
+
   render: function() {
     return (
       <div className="scoreboard">
@@ -127,6 +181,7 @@ var Application = React.createClass({
           {this.state.players.map(function(player, index) {
             return (
               <Player
+                onRemove={function() {this.onRemovePlayer(index)}.bind(this)}
                 onScoreChange={function(delta) {this.onScoreChange(index, delta)}.bind(this)}
                 name={player.name}
                 score={player.score}
@@ -134,6 +189,7 @@ var Application = React.createClass({
             );
           }.bind(this))}
         </div>
+        <AddPlayerForm onAdd={this.onPlayerAdd} />
       </div>
     );
   }
